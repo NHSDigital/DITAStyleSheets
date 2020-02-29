c388d4f675495f159fb541b43439982c73e0347f
Nicholas Oughtibridge <nicholas@ufti.com>
2020-02-29 16:00:51 +0000

    Introduction of example document

---
+++ ExampleDITA/out/webhelp-responsive/oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+define(function () {
+return ["example.html@@@GENERAL MEDICAL PRACTITIONER@@@Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi..."];
+});
