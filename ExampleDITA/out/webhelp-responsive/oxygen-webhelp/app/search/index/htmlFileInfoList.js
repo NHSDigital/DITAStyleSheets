c55cc82b765bb071b58987ec41483f7ff4f529d3
niou <niou@PLTPSWRK082.Corp.Internal>
2020-06-24 15:51:40 +0100

    Refine CSS

--- ExampleDITA/out/webhelp-responsive/oxygen-webhelp/app/search/index/htmlFileInfoList.js
+++
@@ -1,3 +0,0 @@
-define(function () {
-return ["example.html@@@GENERAL MEDICAL PRACTITIONER@@@Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi..."];
-});
--- ExampleDITA/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++
@@ -1,8 +0,0 @@
-define(function() {var keywords=[{w:"GENERAL",p:["p0"]},{w:"MEDICAL",p:["p0"]},{w:"PRACTITIONER",p:["p0"]}];
-var ph={};
-ph["p0"]=[0, 1, 2];
-     return {
-         keywords: keywords,
-         ph: ph
-     }
-});
